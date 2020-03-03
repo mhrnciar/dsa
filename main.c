143198998e4a487651d8f06206e28d991e503820
mhrnciar <matej.hrnciar2@gmail.com>
2020-03-03 18:38:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -8,16 +8,12 @@
 
 void *memory;
 
-void set(unsigned int pos, signed int value){
+void set(unsigned short pos, short value){
     *(short *) (memory + pos) = value;
 }
 
-short get(unsigned int pos){
+short get(unsigned short pos){
     return *(short *) (memory + pos);
-}
-
-void flip(unsigned int pos){
-    set(pos, - get(pos));
 }
 
 void print(int size){
@@ -27,10 +23,18 @@ void print(int size){
     printf("\n");
 }
 
+/*
+ * memory_alloc() hlada volny blok na zaklade best-fit. Najprv sa nastavim na prvy volny blok,
+ * na ktory ukazuje cislo s offsetom 2, teda jeden sizeof(short) a ak je volny blok presne
+ * rovnako velky, tak prejde k alokacii pola. Ak je vacsi a dostatocne velky na to, aby sa dal
+ * rozdelit, zapise rozdiel velkosti a pokracuje na dalsi volny blok. Po prejdeni celeho pola
+ * alokuje blok na mieste, ktory sa velkostou najblizsie rovna pozadovanej velkosti pola. Funkcia
+ * nevracia smernik na hlavicku, ale priamo na prvy vyuzitelny bajt v bloku.
+ */
 void *memory_alloc(unsigned int size){
     int diff = get(0), min = get(0), loc = -1;
     unsigned int p_head = get(sizeof(short));
-    int len = - get(p_head);
+    unsigned int len = get(p_head);
     int next = get(p_head + 2 * sizeof(short));
     
     if(p_head == 0) {
@@ -39,10 +43,12 @@ void *memory_alloc(unsigned int size){
     }
     
     while(1){
+        // Ak sa blok presne zmesti do volneho bloku
         if((len - size) == 0){
             loc = p_head;
             break;
         }
+        // Ak sa blok zmesti do volneho bloku a da sa rozdelit
         else if(len - size >= 0 && len - size > 6 * sizeof(short)){
             diff = len - size;
             if(diff < min){
@@ -50,10 +56,10 @@ void *memory_alloc(unsigned int size){
                 loc = p_head;
             }
         }
-        
+        // Posun na dalsi volny blok
         if(next > 0){
             p_head = next;
-            len = - get(p_head);
+            len = get(p_head);
             next = get(p_head + 2 * sizeof(short));
         }
         else{
@@ -62,35 +68,50 @@ void *memory_alloc(unsigned int size){
     }
     
     if(loc < 0){
-        printf("Nenašiel sa žiadny dostatočne veľký voľný blok\n");
+        printf("Nenašiel sa žiadny dostatočne veľký voľný blok, ktorý by sa dal rozdeliť\n");
         return NULL;
     }
     
     p_head = loc;
-    len = - get(p_head);
+    len = get(p_head);
+    diff = len - size;
     unsigned int p_foot = p_head + len + sizeof(short);
     int prev = get(p_head + sizeof(short));
     next = get(p_head + 2 * sizeof(short));
     
+    // Ak sa pozadovany blok presne zmesti do volneho bloku
     if(diff == 0){
-        flip(p_head);
-        flip(p_foot);
+        set(p_head, size + 1);
+        set(p_foot, size + 1);
+        set(p_head + sizeof(short), prev);
+        set(p_head + 2 * sizeof(short), next);
         
-        set(sizeof(short), next);
+        if(prev < 0)
+            set(sizeof(short), 0);
+        else
+            set(prev + sizeof(short), p_head);
         
         if(next > 0)
             set(next + sizeof(short), prev);
+        
+        for(int i = p_head + 3 * sizeof(short); i < p_foot; i += sizeof(short)){
+            set(i, 0);
+        }
     }
     
+    // Ak sa blok zmesti do volneho bloku a da sa rozdelit
     else{
-        int new_len = - (len - (size + 2 * sizeof(short)));
+        int new_len = len - (size + 2 * sizeof(short));
         unsigned int head = p_head;
         unsigned int foot = p_head + size + sizeof(short);
         
+        // Ak alokovany blok nesiaha na uplny koniec pamate
         if(foot < get(0) - sizeof(short)){
-            set(head, size);
-            set(foot, size);
+            set(head, size + 1);
+            set(foot, size + 1);
             set(foot + sizeof(short), new_len);
+            set(foot + 2 * sizeof(short), prev);
+            set(foot + 3 * sizeof(short), next);
             set(p_foot, new_len);
             
             if(prev < 0)
@@ -104,45 +125,64 @@ void *memory_alloc(unsigned int size){
         }
         
         else{
-            set(head, size);
-            set(foot, size);
+            set(head, size + 1);
+            set(foot, size + 1);
             
             if(prev < 0)
                 set(sizeof(short), 0);
             else
-                set(prev + sizeof(short), 0);
+                set(prev + sizeof(short), head);
             
             
             if (next > 0) {
                 set(next + 2 * sizeof(short), foot + sizeof(short));
             }
         }
+        
+        for(int i = head + 3 * sizeof(short); i < foot; i += sizeof(short)){
+            set(i, 0);
+        }
     }
-    
     return memory + p_head + sizeof(short);
 }
 
+/*
+ * memory_free() dostane smernik na prvy vyuzitelny bajt v bloku, takze hlavicka sa nachadza o
+ * jeden sizeof(short) dozadu. Najprv premeni neparne hodnoty v hlavicke a paticke na parne
+ * cim da najavo ze blok bol dealokovany, a potom spaja bloky podla styroch moznych scenarov:
+ *  1. dealokovany blok je obkoleseny volnymi blokmi: vtedy sa vsetky tri bloky spoja, scitaju
+ *  sa ich velkosti a upravia sa ukazovatele na dalsie volne bloky
+ *  2. za dealokovanym blokom nasleduje volny blok
+ *  3. dealokovanemu bloku predchadza volny blok
+ *  4. okolo nie su ziadne volne bloky
+ * Pri kazdom pripade (okrem 4) sa velkosti blokov scitaju a nova dlzka sa zapise do hlavicky a
+ * paticky noveho velkeho bloku a upravia sa smerniky (okrem pripadu 3), ktore ukazuju na predosle a nasledujuce
+ * volne bloky. Funkcia vrati 0, ak sa dealokacia podarila, 1 ak sa nepodarila.
+ */
 int memory_free(void *valid_ptr){
     if(valid_ptr == NULL){
+        printf("Neplatný smerník\n");
         return 1;
     }
     
     int head = valid_ptr - memory - sizeof(short);
     int size = get(head);
-    int foot = head + size + sizeof(short);
     
-    if(size < 0){
+    if(size % 2 == 0){
         printf("Blok nebol alokovaný\n");
         return 1;
     }
     
-    flip(head);
-    flip(foot);
+    size -= 1;
+    int foot = head + size + sizeof(short);
     
-    if(sizeof(short) == 0){
+    set(head, size);
+    set(foot, size);
+    
+    if(get(sizeof(short)) == 0){
         set(sizeof(short), head);
-        set(valid_ptr - memory, -1);
-        set(valid_ptr - memory + sizeof(short), -1);
+        set(head + sizeof(short), -1);
+        set(head + 2 * sizeof(short), -1);
         
         return 0;
     }
@@ -150,30 +190,33 @@ int memory_free(void *valid_ptr){
     int next_head = foot + sizeof(short);
     int prev_foot = head - sizeof(short);
     
-    if(next_head < get(0) && prev_foot > sizeof(short) && get(prev_foot) < 0 && get(next_head) < 0){
-        int prev_size = - get(prev_foot);
-        int next_size = - get(next_head);
+    // Ak je uvolnovany blok obkoleseny volnymi blokmi
+    if(next_head < get(0) && prev_foot > sizeof(short) && get(prev_foot) % 2 == 0 && get(next_head) % 2 == 0){
+        int prev_size = get(prev_foot);
+        int next_size = get(next_head);
         int new_size = prev_size + size + next_size + 4 * sizeof(short);
         int next_foot = next_head + next_size + sizeof(short);
         int prev_head = prev_foot - prev_size - sizeof(short);
         int next_next = get(next_head + 2 * sizeof(short));
         
-        set(prev_head, - new_size);
-        set(next_foot, - new_size);
+        set(prev_head, new_size);
+        set(next_foot, new_size);
         set(prev_head + 2 * sizeof(short), next_next);
         
         if (next_next > 0) {
-            set(next_next - sizeof(short), prev_head);
+            set(next_next + sizeof(short), prev_head);
         }
-        for(int i = prev_head + 3 * sizeof(short); i < next_foot - sizeof(short); i += 2) {
+        
+        for(int i = prev_head + 3 * sizeof(short); i < next_foot; i += sizeof(short)) {
             set(i, 0);
         }
         
         return 0;
     }
     
-    else if(next_head < get(0) && get(next_head) < 0){
-        int next_size = - get(next_head);
+    // Ak za uvolnovanym blokom nasleduje volny blok
+    else if(next_head < get(0) && get(next_head) % 2 == 0){
+        int next_size = get(next_head);
         int new_size = size + next_size + 2 * sizeof(short);
         int next_foot = next_head + next_size + sizeof(short);
         int next_prev = get(next_head + sizeof(short));
@@ -182,8 +225,8 @@ int memory_free(void *valid_ptr){
         set(head + sizeof(short), next_prev);
         set(head + 2 * sizeof(short), next_next);
         
-        set(head, - new_size);
-        set(next_foot, - new_size);
+        set(head, new_size);
+        set(next_foot, new_size);
         
         if (next_next > 0)
             set(next_next + sizeof(short), head);
@@ -194,31 +237,30 @@ int memory_free(void *valid_ptr){
         if (get(sizeof(short)) == next_head)
             set(sizeof(short), head);
         
-        for(int i = head + 3 * sizeof(short); i < next_foot - sizeof(short); i += 2) {
+        for(int i = head + 3 * sizeof(short); i < next_foot; i += sizeof(short)) {
             set(i, 0);
         }
         
         return 0;
     }
     
-    else if(prev_foot > sizeof(short) && get(prev_foot) < 0){
-        int prev_size = - get(prev_foot);
+    // Ak uvolnovanemu bloku predchadza volny blok
+    else if(prev_foot > sizeof(short) && get(prev_foot) % 2 == 0){
+        int prev_size = get(prev_foot);
         int new_size = size + prev_size + 2 * sizeof(short);
         int prev_head = prev_foot - prev_size - sizeof(short);
         
-        set(prev_head, - new_size);
-        set(head + size + sizeof(short), - new_size);
+        set(prev_head, new_size);
+        set(head + size + sizeof(short), new_size);
         
-        for(int i = prev_head + 3 * sizeof(short); i < head + size; i += 2) {
+        for(int i = prev_head + 3 * sizeof(short); i < head + size; i += sizeof(short)) {
             set(i, 0);
         }
         
         return 0;
     }
     
-    set(head + sizeof(short), -1);
-    set(head + 2 * sizeof(short), -1);
-    
+    // Ak je uvolneny blok prvy v poradi
     if(head < get(sizeof(short))){
         set(get(sizeof(short)) + sizeof(short), head);
         set(head + sizeof(short), -1);
@@ -226,11 +268,11 @@ int memory_free(void *valid_ptr){
         set(sizeof(short), head);
     }
     
-    else{
+    else{   //!!!!!!!!!!!!!!!!!
         int akt = get(sizeof(short));
         
         while(1){
-            int pom = get(akt + sizeof(short));
+            int pom = get(akt + 2 * sizeof(short));
             
             if(pom > 0){
                 if(pom < head){
@@ -259,24 +301,29 @@ int memory_free(void *valid_ptr){
     return 1;
 }
 
+/*
+ * memory_check() skontroluje, ci smernik ukazuje na alokovany blok v pamati.Zistuje to
+ * pomocou hodnoty zapisanej v hlavicke, ak je cislo neparne, blok je alokovany, a teda
+ * funkcia vrati 1. Ak je parne, tak ide o volny blok a funkcia vrati 0.
+ */
 int memory_check(void *ptr){
-    if (ptr > (memory + *(short *) memory)){
-        return 0;
-    }
     int head = get(ptr - memory - sizeof(short));
-    
-    if (head < 0){
-        return 0;
+    if(head % 2 == 1){
+        printf("Smerník ukazuje na alokovaný blok\n");
+        return 1;
     }
-    int foot = get(ptr - memory + head);
-    
-    if (foot < 0 || head != foot){
-        return 0;
-    }
-    
-    return 1;
+    printf("Smerník neukazuje na alokovaný blok\n");
+    return 0;
 }
 
+/*
+ * Pri memory_init() si nasmerujem smernik na pole a urcim si hlavicku, paticku a prvy volny
+ * blok. Na 0-tom mieste je zapisana velkost celeho pola, na offsete 2 je ukazovatel na prvy
+ * volny blok (na zaciatku je to 4). Na offsete 4 je hlavicka volneho bloku, ktora ukazuje
+ * velkost volneho bloku a na offsete size - sizeof(short) je paticka, ktora taktiez ukazuje
+ * velkost volneho bloku. Volne bloky su vyjadrene v parnych cislach a alokovane v nepanych,
+ * konkretne ak alokujem pole velkosti 20, tak v hlavicke bude zapisana hodnota 21.
+ */
 void memory_init(void *ptr, unsigned int size){
     memory = ptr;
     
@@ -284,10 +331,10 @@ void memory_init(void *ptr, unsigned int size){
     
     set(0, size);
     set(sizeof(short), 2 * sizeof(short));
-    set(2 * sizeof(short), -(size - 4 * sizeof(short)));
+    set(2 * sizeof(short), (size - 4 * sizeof(short)));
     set(3 * sizeof(short), -1);
     set(4 * sizeof(short), -1);
-    set(get(0) - sizeof(short), -(size - 4 * sizeof(short)));
+    set(get(0) - sizeof(short), (size - 4 * sizeof(short)));
     
     for(int i = 5 * sizeof(short); i < size - sizeof(short); i += sizeof(short)){
         set(i, 0);
@@ -298,24 +345,26 @@ void memory_init(void *ptr, unsigned int size){
 
 int main() {
     char region[100];
-    memory_init(region, 100);
-    
     int size = sizeof(region) / sizeof(region[0]);
     
-    char *pointer = (char*) memory_alloc(10);
+    memory_init(region, 100);
+    
+    char *pointer = (char*) memory_alloc(30);
     print(size);
-    char *p = (char *) memory_alloc(20);
+    char *pointer2 = (char*) memory_alloc(20);
     print(size);
-    char *p2 = (char *) memory_alloc(20);
+    char *pointer3 = (char*) memory_alloc(10);
     print(size);
-    char *p3 = (char *) memory_alloc(20);
+    char *pointer4 = (char*) memory_alloc(20);
     print(size);
     
-    memory_free(p);
+    memory_check(pointer3);
+    
+    memory_free(pointer2);
     print(size);
-    memory_free(p3);
+    memory_free(pointer4);
     print(size);
-    char *p4 = (char *) memory_alloc(20);
+    memory_free(pointer3);
     print(size);
     
     if(pointer)
